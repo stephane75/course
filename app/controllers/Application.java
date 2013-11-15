3302ff44a5bcb73112c46728822c1926a37cc986
Stéphane Desplas <desplas.s@sfeir.com>
2013-11-15 18:06:57 +0100

    init project

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,8 +9,20 @@ import models.*;
 
 public class Application extends Controller {
 
-    public static void index() {
+    public static void home() {
         render();
+    }
+
+    public static void produits() {
+        render("Application/produits.html");
+    }
+
+    public static void categories() {
+        render("Application/categories.html");
+    }
+
+    public static void courses() {
+        render("Application/courses.html");
     }
 
 }
